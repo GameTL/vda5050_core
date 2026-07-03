ff5a3d2fbe82defa7d70c43a6fcc3c175b777c1c
Saurabh Kamat <saurabh_kamat@a-star.edu.sg>
2026-07-03 17:31:16 +0800

    feat: refactor action request
    Signed-off-by: Saurabh Kamat <saurabh_kamat@a-star.edu.sg>

--- vda5050_core/include/vda5050_core/client/adapter/action_request.hpp
+++ vda5050_core/include/vda5050_core/client/adapter/action_request.hpp
@@ -19,7 +19,13 @@
 #ifndef VDA5050_CORE__CLIENT__ADAPTER__ACTION_REQUEST_HPP_
 #define VDA5050_CORE__CLIENT__ADAPTER__ACTION_REQUEST_HPP_
 
+#include <cstdint>
+#include <optional>
+#include <string>
+#include <vector>
+
 #include "vda5050_core/types/action.hpp"
+#include "vda5050_core/types/action_parameter.hpp"
 
 namespace vda5050_core {
 
@@ -27,9 +33,43 @@ namespace client {
 
 namespace adapter {
 
-struct ActionRequest
+class ActionRequest
 {
-  types::Action action;
+public:
+  const std::string& action_id() const;
+
+  const std::string& action_type() const;
+
+  const std::optional<std::vector<types::ActionParameter>>& action_parameters()
+    const;
+
+  const std::optional<std::string>& action_description() const;
+
+  const std::optional<std::string>& order_id() const;
+
+  std::optional<uint32_t> order_update_id() const;
+
+private:
+  friend class Adapter;
+
+  static ActionRequest from_action(const types::Action& action);
+
+  ActionRequest(
+    const std::string& action_id, const std::string& action_type,
+    std::optional<std::vector<types::ActionParameter>> action_parameters =
+      std::nullopt,
+    std::optional<std::string> action_description = std::nullopt);
+
+  void add_order_information(
+    const std::string& order_id, uint32_t order_update_id);
+
+  std::string action_id_;
+  std::string action_type_;
+  std::optional<std::vector<types::ActionParameter>> action_parameters_;
+  std::optional<std::string> action_description_;
+
+  std::optional<std::string> order_id_;
+  std::optional<uint32_t> order_update_id_;
 };
 
 }  // namespace adapter
